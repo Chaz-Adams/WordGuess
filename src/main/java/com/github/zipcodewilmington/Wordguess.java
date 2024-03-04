77e038c6fb84242591a7a33ee5b1120bb0f683d5
Chaz-Adams <caadams1922@gmail.com>
2024-03-04 17:10:17 -0500

    started over, having trouble with the logic

--- src/main/java/com/github/zipcodewilmington/Wordguess.java
+++ src/main/java/com/github/zipcodewilmington/Wordguess.java
@@ -1,5 +1,7 @@
 package com.github.zipcodewilmington;
 
+import java.util.Arrays;
+import java.util.Random;
 import java.util.Scanner;
 import java.lang.Math.*;
 
@@ -8,50 +10,14 @@ import java.lang.Math.*;
  */
 public class Wordguess {
 
+    static Scanner scanner = new Scanner(System.in);
+
     public static void main(String[] args){
-//        Scanner scanner = new Scanner(System.in);
-//
-//        String userGuess;
-//        int numOfGuesses = 4;
-//
-//        while(numOfGuesses <6){
-//            /*
-//             I need to set up an array and then search through the array
-//             if it matches one of the elements in the array we need to replace
-//             the element position '_' with the letter
-//             after every guess check to see if the word is complete or if ran out
-//             of guesses
-//
-//             I want to have  the loop call methods such as the menu or the compare etc.*/
-//            System.out.print("Let's Play Wordguess version 1.0");
-//            System.out.println("Current Guesses: ");
-//            System.out.println("_ _ _ _");
-//            System.out.println("You have "+numOfGuesses+" tries left.");
-//            System.out.println("Enter a single character: ");
-//
-//            //write some exception handling incase its more than one char
-//            userGuess = scanner.next();
-        //}
-
-        Wordguess main = new Wordguess();
-
-        int randomNumber = main.getRandomNumber();
-        String randomWord = main.getRandomWord(randomNumber);
-
-        main.runGame(randomWord);
-
-
-
-        System.out.println(randomWord);
-
+        Wordguess game = new Wordguess();
+        game.runGame();
     }
 
-    public  int getRandomNumber(){
-        int max = 10;
-        int min = 1;
-        // Generate random number within the range
-        return (int) ((Math.random() * (max - min)) + min);
-    }
+    public Wordguess() {};
 
     public String getRandomWord(int randomNumber){
 
@@ -61,22 +27,74 @@ public class Wordguess {
         return wordArray[randomNumber];
     }
 
-    public void runGame(String randomWord){
-        int numberOfTries = randomWord.length()+3;
+    public void runGame() {
+
+        Random r = new Random();
+        int random_int = r.nextInt(10) + 1;
+        String magicWord = getRandomWord(random_int);
+        int numOfTries = 0;
+        int allowedTries = magicWord.length();
+        boolean isSolved = false;
 
         System.out.println("Let's Play Wordguess version 1.0");
-        System.out.println("Current Guesses:");
-        underScores(randomWord);
-        System.out.println();
-        System.out.println("You have "+numberOfTries+" tries left.");
-        System.out.println("Enter a single character: ");
+        char[] solution = magicWord.toCharArray();
+        char[] guesses = new char[magicWord.length()];
+
+        for (int i = 0; i < magicWord.length(); i++) {
+            System.out.print("- ");
+        }
+
+
+
+        while (numOfTries < allowedTries && isSolved == false) {
+            System.out.println("Current Guesses:");
+
+            String guess = scanner.next();
+            if (guess == "-") {
+                System.out.println("\n\n\t\tExiting the game..");
+                System.exit(0);
+            } else guesses = guess.toCharArray();
+
+
+        }
+
+
+        System.out.println("Magic Word is: "+magicWord);
+        System.out.println("Solution Array: "+solution.toString());
+        System.out.println("Guesses: "+ guesses.toString());
+
 
     }
 
     public void underScores(String randomWord){
-        for(int i = 0; i < randomWord.length();i++){
-            System.out.print("_");
-        }
+
     }
 
+    public String newUnderScoreWord(){
+        return null;
+    }
+
+    public int isGuessCorrect(char userGuess, String magicWord){
+        int correctUserGuess = 0;
+
+        char[] charArray = magicWord.toCharArray();
+        for(int i = 0; i < magicWord.length(); i++){
+            if(charArray[i] == userGuess){
+                System.out.println("That letter is in the word!");
+                correctUserGuess = i;
+                System.out.println("Found in index: "+correctUserGuess);
+            }else {
+                System.out.println("That letter is NOT in the word!");
+                // go back to loop and say something about wrong guess
+            }
+        }
+        return correctUserGuess;
+    }
+
+
+
 }
+
+
+
+
